d08311b5e1f714249c4488e751b7165f46cd5076
Jacob Cattrall <krazyjakee@gmail.com>
2018-12-08 00:34:43 +0100

    update and audit

--- example/example.dist.js
+++ example/example.dist.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
 var _core = _interopRequireDefault(require("@keyframes/core"));
@@ -38,7 +38,7 @@ window.onload = function () {
   b3.play('circular 5s linear 0s infinite');
 };
 
-},{"../src/keyframes.pathfinder":3,"@keyframes/core":2}],2:[function(require,module,exports){
+},{"../src/keyframes.pathfinder":5,"@keyframes/core":2}],2:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -51,6 +51,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+require('es6-object-assign/auto');
 
 var Keyframes =
 /*#__PURE__*/
@@ -69,11 +71,12 @@ function () {
   }, {
     key: "reset",
     value: function reset(callback) {
+      this.removeEvents();
       this.elem.style.animationPlayState = 'running';
       this.elem.style.animation = 'none';
 
       if (callback) {
-        setTimeout(callback, 0);
+        requestAnimationFrame(callback);
       }
     }
   }, {
@@ -91,33 +94,14 @@ function () {
     value: function play(frameOptions, callback) {
       var _this = this;
 
-      var animObjToStr = function animObjToStr(obj) {
-        var newObj = Object.assign({}, {
-          duration: '0s',
-          timingFunction: 'ease',
-          delay: '0s',
-          iterationCount: 1,
-          direction: 'normal',
-          fillMode: 'forwards'
-        }, obj);
-        return [newObj.name, newObj.duration, newObj.timingFunction, newObj.delay, newObj.iterationCount, newObj.direction, newObj.fillMode].join(' ');
-      };
-
-      var animationcss = '';
-
-      if (frameOptions.constructor === Array) {
-        var frameOptionsStrings = [];
-
-        for (var i = 0; i < frameOptions.length; i += 1) {
-          frameOptionsStrings.push(typeof frameOptions[i] === 'string' ? frameOptions[i] : animObjToStr(frameOptions[i]));
-        }
-
-        animationcss = frameOptionsStrings.join(', ');
-      } else if (typeof frameOptions === 'string') {
-        animationcss = frameOptions;
-      } else {
-        animationcss = animObjToStr(frameOptions);
+      if (this.elem.style.animationName === frameOptions.name) {
+        this.reset(function () {
+          return _this.play(frameOptions, callback);
+        });
+        return this;
       }
+
+      var animationcss = Keyframes.playCSS(frameOptions);
 
       var addEvent = function addEvent(type, eventCallback) {
         var listenerName = "".concat(type, "Listener");
@@ -134,22 +118,49 @@ function () {
       this.frameOptions = frameOptions;
       addEvent('animationiteration', callback || frameOptions.complete);
       addEvent('animationend', callback || frameOptions.complete);
-    }
-  }], [{
-    key: "createKeyframeTag",
-    value: function createKeyframeTag(id, css) {
-      var elem = document.createElement('style');
-      elem.innerHTML = css;
-      elem.setAttribute('class', 'keyframe-style');
-      elem.setAttribute('id', id);
-      elem.setAttribute('type', 'text/css');
-      document.getElementsByTagName('head')[0].appendChild(elem);
+      return this;
     }
   }, {
-    key: "generate",
-    value: function generate(frameData) {
-      var frameName = frameData.name || '';
-      var css = "@keyframes ".concat(frameName, " {");
+    key: "removeEvents",
+    value: function removeEvents() {
+      this.elem.removeEventListener('animationiteration', this.animationiterationListener);
+      this.elem.removeEventListener('animationend', this.animationendListener);
+    }
+  }], [{
+    key: "playCSS",
+    value: function playCSS(frameOptions) {
+      var animObjToStr = function animObjToStr(obj) {
+        var newObj = Object.assign({}, {
+          duration: '0s',
+          timingFunction: 'ease',
+          delay: '0s',
+          iterationCount: 1,
+          direction: 'normal',
+          fillMode: 'forwards'
+        }, obj);
+        return [newObj.name, newObj.duration, newObj.timingFunction, newObj.delay, newObj.iterationCount, newObj.direction, newObj.fillMode].join(' ');
+      };
+
+      if (frameOptions.constructor === Array) {
+        var frameOptionsStrings = [];
+
+        for (var i = 0; i < frameOptions.length; i += 1) {
+          frameOptionsStrings.push(typeof frameOptions[i] === 'string' ? frameOptions[i] : animObjToStr(frameOptions[i]));
+        }
+
+        return frameOptionsStrings.join(', ');
+      }
+
+      if (typeof frameOptions === 'string') {
+        return frameOptions;
+      }
+
+      return animObjToStr(frameOptions);
+    }
+  }, {
+    key: "generateCSS",
+    value: function generateCSS(frameData) {
+      var css = "@keyframes ".concat(frameData.name, " {");
 
       for (var key in frameData) {
         if (key !== 'name' && key !== 'media' && key !== 'complete') {
@@ -163,17 +174,27 @@ function () {
         }
       }
 
+      css += '}';
+
       if (frameData.media) {
         css = "@media ".concat(frameData.media, "{").concat(css, "}");
       }
 
-      var frameStyle = document.getElementById(frameName);
+      return css;
+    }
+  }, {
+    key: "generate",
+    value: function generate(frameData) {
+      var css = this.generateCSS(frameData);
+      var oldFrameIndex = Keyframes.rules.indexOf(frameData.name);
 
-      if (frameStyle) {
-        frameStyle.innerHTML = css;
-      } else {
-        Keyframes.createKeyframeTag(frameName, css);
+      if (oldFrameIndex > -1) {
+        Keyframes.sheet.deleteRule(oldFrameIndex);
+        delete Keyframes.rules[oldFrameIndex];
       }
+
+      var ruleIndex = Keyframes.sheet.insertRule(css, 0);
+      Keyframes.rules[ruleIndex] = frameData.name;
     }
   }, {
     key: "define",
@@ -187,18 +208,105 @@ function () {
       }
     }
   }, {
+    key: "defineCSS",
+    value: function defineCSS(frameData) {
+      if (frameData.length) {
+        var css = '';
+
+        for (var i = 0; i < frameData.length; i += 1) {
+          css += this.generateCSS(frameData[i]);
+        }
+
+        return css;
+      }
+
+      return this.generateCSS(frameData);
+    }
+  }, {
     key: "plugin",
     value: function plugin(pluginFunc) {
-      pluginFunc(Keyframes);
+      if (pluginFunc.constructor === Array) {
+        for (var i = 0; i < pluginFunc.length; i += 1) {
+          pluginFunc[i](Keyframes);
+        }
+      } else {
+        pluginFunc(Keyframes);
+      }
     }
   }]);
 
   return Keyframes;
 }();
 
-exports.default = Keyframes;
+if (typeof document !== 'undefined') {
+  var style = document.createElement('style');
+  style.setAttribute('id', 'keyframesjs-stylesheet');
+  document.head.appendChild(style);
+  Keyframes.sheet = style.sheet;
+  Keyframes.rules = [];
+}
 
-},{}],3:[function(require,module,exports){
+var _default = Keyframes;
+exports.default = _default;
+
+},{"es6-object-assign/auto":3}],3:[function(require,module,exports){
+'use strict';
+
+require('./index').polyfill();
+
+},{"./index":4}],4:[function(require,module,exports){
+/**
+ * Code refactored from Mozilla Developer Network:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+ */
+'use strict';
+
+function assign(target, firstSource) {
+  if (target === undefined || target === null) {
+    throw new TypeError('Cannot convert first argument to object');
+  }
+
+  var to = Object(target);
+
+  for (var i = 1; i < arguments.length; i++) {
+    var nextSource = arguments[i];
+
+    if (nextSource === undefined || nextSource === null) {
+      continue;
+    }
+
+    var keysArray = Object.keys(Object(nextSource));
+
+    for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+      var nextKey = keysArray[nextIndex];
+      var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+
+      if (desc !== undefined && desc.enumerable) {
+        to[nextKey] = nextSource[nextKey];
+      }
+    }
+  }
+
+  return to;
+}
+
+function polyfill() {
+  if (!Object.assign) {
+    Object.defineProperty(Object, 'assign', {
+      enumerable: false,
+      configurable: true,
+      writable: true,
+      value: assign
+    });
+  }
+}
+
+module.exports = {
+  assign: assign,
+  polyfill: polyfill
+};
+
+},{}],5:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
